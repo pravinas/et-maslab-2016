7285d24d231a0a07e27cb850578c3ee3e4aa1077
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.MIT.EDU>
2016-01-04 15:30:35 -0500

    Made the publisher framework for the bot.

--- src/pubsub/pubsub.py
+++ src/pubsub/pubsub.py
@@ -1,5 +1,35 @@
-// pubsub.py
-//
-// Publisher-Subscriber framework for MASLAB 2016 Team 6.
+## pubsub.py
+#
+# Publisher-Subscriber framework for MASLAB 2016 Team 6.
 
-class Publisher
+class Publisher:
+
+    def __init__(self):
+        self.subscribers = []   # A list of callback functions subscribed to this publisher.
+        self.most_recent = null # The most recent message sent to this publisher.
+
+    ## Do something with a message when sent
+    # 
+    # @param callback_fn The function to be carried out when the message is recieved.
+    def subscribe(self, callback_fn):
+        self.subscribers.append(callback_fn)
+
+    ## Send a message
+    #
+    # @param message The message to be sent
+    def publish(self, message):
+        most_recent = message
+        notify(message)
+
+    ## Get the most recent message sent.
+    #
+    # @return The most recent message
+    def get_most_recent(self):
+        return most_recent
+
+    ## Run all callback functions on the message
+    #
+    # @param message The message to process.
+    def notify(self, message):
+        for (callback : subscribers):
+            callback(message)
