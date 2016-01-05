fafb8ed617438d5b18a6faae848db35a08675f61
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.MIT.EDU>
2016-01-05 13:28:08 -0500

    Made a main file that uses TAMProxy's python bindings.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-// placeholders
+# main.py
+#
+# The main sketch for the robot's processes.
+
+from tamproxy import Sketch
+
+class Robot(Sketch):
+
+	def setup(self):
+		# TODO: Initialization
+		pass
+
+	def loop(self):
+		# TODO
+		pass
