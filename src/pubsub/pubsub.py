b6428fb1cc3da68b1d200b8d8b987de77cf0630b
Pravina Samaratunga <pravina@dhcp-18-189-22-55.dyn.MIT.EDU>
2016-01-04 19:25:58 -0500

    Some progress on CV things.

--- src/pubsub/pubsub.py
+++ src/pubsub/pubsub.py
@@ -6,7 +6,7 @@ class Publisher:
 
     def __init__(self):
         self.subscribers = []   # A list of callback functions subscribed to this publisher.
-        self.most_recent = null # The most recent message sent to this publisher.
+        self.most_recent = None # The most recent message sent to this publisher.
 
     ## Do something with a message when sent
     # 
@@ -18,18 +18,18 @@ class Publisher:
     #
     # @param message The message to be sent
     def publish(self, message):
-        most_recent = message
-        notify(message)
+        self.most_recent = message
+        self.notify(message)
 
     ## Get the most recent message sent.
     #
     # @return The most recent message
     def get_most_recent(self):
-        return most_recent
+        return self.most_recent
 
     ## Run all callback functions on the message
     #
     # @param message The message to process.
     def notify(self, message):
-        for (callback : subscribers):
+        for callback in self.subscribers:
             callback(message)
--- src/vision/vision.py
+++ src/vision/vision.py
@@ -1,1 +1,78 @@
-// placeholders
+import cv2
+import numpy as np
+
+class Color():
+    def __init__(self, h_min, h_max, s_min, s_max, v_min, v_max):
+        self.h_min = h_min
+        self.h_max = h_max
+        self.s_min = s_min
+        self.s_max = s_max
+        self.v_min = v_min
+        self.v_max = v_max
+
+class BlockImg():
+    def __init__(self, x, y, area):
+        self.x = x
+        self.y = y
+        self.area = area
+
+BLACK = Color(0, 255, 20, 255, 0, 20)
+WHITE = Color(0, 255, 0, 25, 240, 255)
+BLUE  = Color(100, 140, 100, 200, 50, 255)
+GREEN = Color(37, 96, 50, 255, 40 255)
+RED  = Color(150, 15, 50, 255, 50, 255)
+
+class Vision():
+    def __init__(self):
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.CV_CAP_PROP_FRAME_WIDTH, 320); # X resolution
+        self.capture.set(cv2.CV_CAP_PROP_FRAME_HEIGHT, 240); # Y resolution
+
+    def filterHSV(self, img, color):
+        if (color.h_min > color.h_max):
+            out1 = cv2.inRange(img, np.array([color.h_max, color.s_min, color.v_min]), np.array([179, color.s_max, color.v_max]))
+            out2 = cv2.inRange(img, np.array([0, color.s_min, color.v_min]), np.array([color.h_min, color.s_max, color.v_max]))
+            return cv2.bitwise_or(out1, out2)
+        else:
+            return cv2.inRange(img, np.array([color.h_min, color.s_min, color.v_min]), np.array([color.h_max, color.s_max, color.v_max]))
+
+    def morph(self, img):
+        kernel = np.ones((5,5), np.uint8)
+        return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+    def findBlocksInBinaryImage(self, img):
+        image, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        blocks = []
+
+        for contour in contours:
+            moment = c2.moments(contour)
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+            area = cv2.contourArea(cnt)
+            blocks.append(Block(x, y, area))
+
+    def loop(self):
+        retval, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        greenImg = self.filterHSV(frame, GREEN)
+        redImg   = self.filterHSV(frame, RED)
+
+        greenImg = self.morph(greenImg)
+        redImg   = self.morph(redImg)
+
+        greenBlocks = self.findBlocksInBinaryImage(greenImg)
+        redBlocks   = self.findBlocksInBinaryImage(redImg)
+
+        return (greenBlocks, redBlocks)
+
+    def isScreenBlack(self):
+        retval, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        blackImg = self.filterHSV(frame, BLACK)
+        blackImg = self.morph(blackImg)
+
+        return len(cv2.findContours(blackImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[2]) < 2
+
+
+        
