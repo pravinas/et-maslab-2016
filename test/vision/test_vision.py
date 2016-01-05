a31cc89f476b3c373284303f13cf37eb3904c6de
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.MIT.EDU>
2016-01-05 16:47:36 -0500

    Updated the color sensor file to make it doxygen compatible.

--- test/vision/test_vision.py
+++ test/vision/test_vision.py
@@ -1,1 +1,8 @@
 # test_vision.py
+#
+# Tests some functions in vision.py.
+
+# TODO: Write the following code.
+# __init__ only once, before running anything else.
+# Check isScreenBlack() before continuing.
+# loop() continuously with a webcam.
