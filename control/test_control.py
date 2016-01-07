308f363b15a1e7ffc9766f03be16fecbef4528bf
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.MIT.EDU>
2016-01-07 12:19:01 -0500

    Made a skeleton Control test file.

---
+++ control/test_control.py
@@ -0,0 +1,14 @@
+# test_control.py
+# 
+# Run a sketch that tells the robot to run various control routines.
+
+import tamproxy
+
+class TestControl(Sketch):
+	def setup(self):
+		# TODO
+		pass
+
+	def loop(self):
+		# TODO
+		pass
