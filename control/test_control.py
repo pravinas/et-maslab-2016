314c2c408e1a601b4ac485ad5497f466ff4b4f3a
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.MIT.EDU>
2016-01-08 10:18:25 -0500

    Fixed a small syntax error.

--- control/test_control.py
+++ control/test_control.py
@@ -5,27 +5,27 @@
 import tamproxy
 
 class TestControl(Sketch):
-	def setup(self):
-		# just used the code from test_motor for this part
-		# assume motor 1 is left and motor 2 is right side
-		# just connects to motors not sure what else to do here
-		# todo
-		self.motor1 = Motor(self.tamp, 3, 4)
+    def setup(self):
+        # just used the code from test_motor for this part
+        # assume motor 1 is left and motor 2 is right side
+        # just connects to motors not sure what else to do here
+        # todo
+        self.motor1 = Motor(self.tamp, 3, 4)
         self.motor1.write(1,0)
         self.motor2 = Motor(self.tamp, 5, 6)
         self.motor2.write(1,0)
         self.delta = 1
         self.motorval = 0
         self.timer = Timer()
-		pass
+        pass
 
-	def loop(self):
-		# motor1 = left
-		# motor2 = right
-		# using graphical representation Ariel gave us theta v time to think about problem
-		# same timer as previous need to tweak
-		# todo
-		if (self.timer.millis() > 10):
+    def loop(self):
+        # motor1 = left
+        # motor2 = right
+        # using graphical representation Ariel gave us theta v time to think about problem
+        # same timer as previous need to tweak
+        # todo
+        if (self.timer.millis() > 10):
             self.timer.reset()
 
             # θ > 0 means need to turn left
@@ -42,17 +42,17 @@ class TestControl(Sketch):
             if θ > 0:
             # need to make motor2 faster for some value
             # todo
-            	self.motor1.write(0, c )
-            	self.motor2.write(1, c + f)
+                self.motor1.write(0, c )
+                self.motor2.write(1, c + f)
             elif θ < 0:
             # need to make motor 1 faster for some value
             #todo
-            	self.motor1.write(0, c + f)
-            	self.motor2.write(1, c)
+                self.motor1.write(0, c + f)
+                self.motor2.write(1, c)
             else:
             # need to decide on some magnitude c
             #todo
-            	self.motor1.write(0, c)
-            	self.motor2.write(1, c)
+                self.motor1.write(0, c)
+                self.motor2.write(1, c)
 
-		pass
+        pass
