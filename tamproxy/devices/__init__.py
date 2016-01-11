b7bde6ce0e6f748644612e05193a3270772ec8a9
Pravina Samaratunga <pravina@dhcp-18-189-81-103.dyn.mit.edu>
2016-01-11 12:21:58 -0500

    Removed redundant and confusing and outdated directory.

--- tamproxy/devices/__init__.py
+++
@@ -1,5 +0,0 @@
-from .digital_input import DigitalInput
-from .digital_output import DigitalOutput
-from .analog_input import AnalogInput
-from .analog_output import AnalogOutput
-from .motor import Motor
