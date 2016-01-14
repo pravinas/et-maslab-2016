c961b72188a088de00afef0d00f14e607f36fbea
Pravina Samaratunga <pravinas@mit.edu>
2016-01-14 18:38:13 -0500

    Pulled tests from TAMProxy-pyHost for servo and color sensor.

--- firmware/test_servo.py
+++ firmware/test_servo.py
@@ -6,10 +6,9 @@ class ServoWrite(Sketch):
     """Cycles a servo back and forth between 1050us and 1950us pulse widths (most servos are 1000-2000)"""
 
     def setup(self):
-        self.servo = Servo(self.tamp, 18)
+        self.servo = Servo(self.tamp, 9)
         self.servo.write(1050)
         self.timer = Timer()
-        self.fulltime = Timer()
         self.end = False
 
     def loop(self):
@@ -20,12 +19,6 @@ class ServoWrite(Sketch):
             else:
                 self.servo.write(1950)
             self.end = not self.end
-        if self.fulltime > 20000:
-            self.stop()
-            
-    def stop(self):
-        super(EncoderRead,self).stop()
-        self.tamp.clear_devices()
 
 if __name__ == "__main__":
     sketch = ServoWrite()
