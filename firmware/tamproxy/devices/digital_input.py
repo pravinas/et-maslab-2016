7af898eb30afaeb361ce487893d713a783df7b0e
Pravina Samaratunga <pravinas@mit.edu>
2016-01-19 17:01:29 -0500

    Made some mistakes

---
+++ firmware/tamproxy/devices/digital_input.py
@@ -0,0 +1,34 @@
+from .device import ContinuousReadDevice
+from .. import config as c
+
+class DigitalInput(ContinuousReadDevice):
+
+    DEVICE_CODE =   c.devices.digital_input.code
+    READ_CODE =     c.devices.digital_input.read_code
+
+    def __init__(self, tamproxy, pin, pullup=True, continuous=True):
+        self.pin = pin
+        self.pullup = pullup
+        self.val = 0
+        self.prev_val = None
+        super(DigitalInput, self).__init__(tamproxy, continuous)
+
+    def __repr__(self):
+        return super(DigitalInput, self).__repr__(self.pin)
+
+    @property
+    def add_payload(self):
+        return self.DEVICE_CODE + chr(self.pin) + chr(self.pullup)
+
+    @property
+    def changed(self):
+        if self.prev_val is None:
+            self.prev_val = self.val
+            return False
+        if self.val != self.prev_val:
+            self.prev_val = self.val
+            return True
+        else: return False
+
+    def _handle_update(self, request, response):
+        self.val = ord(response)
---
+++ firmware/tamproxy/devices/encoder.py
@@ -0,0 +1,43 @@
+from .device import ContinuousReadDevice
+from .. import config as c
+
+import ctypes
+
+class Encoder(ContinuousReadDevice):
+
+    DEVICE_CODE =   c.devices.encoder.code
+    WRITE_CODE =    c.devices.encoder.write_code
+    READ_CODE  =    c.devices.encoder.read_code
+
+    def __init__(self, tamproxy, pin_a, pin_b, continuous=True):
+        self.pin_a = pin_a
+        self.pin_b = pin_b
+        self.val = 0
+        super(Encoder, self).__init__(tamproxy, continuous)
+
+    def __repr__(self):
+        return super(Encoder, self).__repr__(self.pin_a, self.pin_b)
+
+    @property
+    def add_payload(self):
+        return self.DEVICE_CODE + chr(self.pin_a) + chr(self.pin_b)
+
+    def write(self, value):
+        value = int(value) & 0xFFFFFFFF
+        self.tamp.send_request(self.id,
+                               self.WRITE_CODE +
+                               chr((int(value) >> 24) & 0xFF) +
+                               chr((int(value) >> 16) & 0xFF) +
+                               chr((int(value) >> 8) & 0xFF) +
+                               chr(int(value) & 0xFF))
+
+    def _handle_update(self, request, response):
+        new_val = (
+            (ord(response[0])<<24) |
+            (ord(response[1])<<16) |
+            (ord(response[2])<<8) |
+            ord(response[3])
+        )
+
+        # this deals with unsigned overflow
+        self.val += ctypes.c_int32(new_val - self.val).value
