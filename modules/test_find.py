274c8c5b21047156f9ec8ba57e126f19d7f6a6e6
Pravina Samaratunga <pravinas@mit.edu>
2016-01-22 18:46:43 -0500

    Cleaned up and streamlined test_find

--- modules/test_find.py
+++ modules/test_find.py
@@ -1,74 +1,29 @@
 from tamproxy import Timer, SyncedSketch
 from module import Module
+from find import FindModule
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from control import GoStraight
 from logic import Logic
+from vision import Vision
 from constants import *
-
 
 class TestFind(SyncedSketch):
 
     def setup(self):
-        # Timeout to make sure we don't run over.
-        self.timeout = 7000
+        leftMotor = Motor(self.tamp, LEFT_DRIVE_CONTROLLER_DIRECTION, LEFT_DRIVE_CONTROLLER_PWM)
+        rightMotor = Motor(self.tamp, RIGHT_DRIVE_CONTROLLER_DIRECTION, RIGHT_DRIVE_CONTROLLER_PWM)
+        vision = Vision(RED, CAMERA_WIDTH, CAMERA_HEIGHT)
+        logic = Logic()
 
-        # Module timer
-        self.timer = Timer()
-
-        self.leftMotor = Motor(self.tamp, LEFT_DRIVE_CONTROLLER_DIRECTION, LEFT_DRIVE_CONTROLLER_PWM)
-        self.rightMotor = Motor(self.tamp, RIGHT_DRIVE_CONTROLLER_DIRECTION, RIGHT_DRIVE_CONTROLLER_PWM)
-        # GoStraight object to control movement
-        self.movement = GoStraight(self.leftMotor, self.rightMotor, Timer())
-
-        # Timer object describing how much time has passed since the last 
-        # camera input was processed.
-        self.cameraTimer = Timer()
-        # Time in milliseconds between pictures being taken.
-        self.cameraTimeout = 500
-        # Vision object to read data from the camera.
-        self.vision = Vision()
-
-        # Logic object for calculations
-        self.logic = Logic(True, 80, 60, debug=False) #False = green, True = red
-
-        self.startThing()
+        self.find = FindModule(Timer(), leftMotor, rightMotor, vision, logic)
+        self.find.start()
 
     def loop(self):
-        self.runThing()
-
-    def startThing(self):
-        self.target = None
-        self.updateTime = 0
-        self.movement.reset()
-        self.timer.reset()
-
-    def runThing(self):   
-        # Allow timeout.
-        if self.timer.millis() > self.timeout:
-            print "Timed out from FIND to FOLLOW"
-            return MODULE_FOLLOW
-
-        # Check if we need to exit the module.
-        if self.logic.checkForBlock() > 0 : 
-            print "Going from FIND to PICKUP"
-            return MODULE_PICKUP
-
-        ## Capture an image from the camera every so often
-        if self.cameraTimer.millis() > self.cameraTimeout:
-            self.cameraTimer.reset()
-
-            self.target = self.logic.findTarget(*self.vision.processImage())
-            self.updateTime = self.timer.millis()
-
-        # Check if we see anything of interest on the screen.
-        if target == None:
-            self.movement.move_to_target(arbitraryTarget)
-            # TODO: Make sure not to enter this subroutine when there is a block in the blind spot.
-        else:
-            self.movement.move_to_target(target)
-            self.logic.bayesianTargetUpdate(target, self.timer.millis() - self.updateTime)
+        response = self.find.run()
+        if response != MODULE_FIND:
+            self.stop()
 
 
 if __name__ == "__main__":
