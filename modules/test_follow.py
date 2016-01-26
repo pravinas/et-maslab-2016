683ca2ed4430bd4da7c3cb53a53f70bcbad8ef22
lotta <l2gb28@gmail.com>
2016-01-26 14:01:38 -0500

    added a limit switch to test follow

--- modules/test_follow.py
+++ modules/test_follow.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from tamproxy import Timer, SyncedSketch
-from tamproxy.devices import Motor, Color, Encoder
+from tamproxy.devices import Motor, DigitalInput
 from follow import FollowModule
 from control.long_range_ir import LRIR
 from control.Wall_Follow import WallFollow
@@ -21,8 +21,8 @@ class TestFollow(SyncedSketch):
         irBL = LRIR(self.tamp, LONG_DISTANCE_IR_BL)
         irBR = LRIR(self.tamp, LONG_DISTANCE_IR_BR)
         wallFollow = WallFollow(leftMotor, rightMotor, wallFollowTimer, irFL, irFR, irBL, irBR)
-        forwardSpeed = -50
-        self.follow = FollowModule(timer, stepTimer, leftMotor, rightMotor, wallFollow, forwardSpeed, blockSwitch)
+        blockSwitch = DigitalInput(self.tamp, BLOCK_LIMIT_SWITCH)
+        self.follow = FollowModule(timer, stepTimer, leftMotor, rightMotor, wallFollow, FORWARD_SPEED, blockSwitch)
         self.follow.start()
 
     def loop(self):
